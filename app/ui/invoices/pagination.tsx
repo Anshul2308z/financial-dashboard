d0c8eca054e52f7119093e9640a69a72c9d5519c
Anshul <nemaanshul42@gmail.com>
2026-01-12 18:58:43 +0530

    Done with paginations/ 10/15. It took a considerable amount to time as it was new to me. In a nutshell: I used useSearchParams, useRouter, usPathname on the client side. This type of pagination is called offset pagination. I also came across cursor pagination while researching a bit.

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -5,7 +5,19 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 
+import {usePathname, useSearchParams} from 'next/navigation';
+
 export default function Pagination({ totalPages }: { totalPages: number }) {
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const currentPage = Number( searchParams.get('page') ) || 1;
+
+  function createPageURL(pageNumber: number | string) {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
   // NOTE: Uncomment this code in Chapter 10
 
   // const allPages = generatePagination(currentPage, totalPages);
